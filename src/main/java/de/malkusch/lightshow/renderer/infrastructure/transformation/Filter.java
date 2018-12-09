8109e5c7fa3cc01a3b2573f322c8cadc92974fc9
Markus Malkusch <markus@malkusch.de>
2018-12-09 21:43:47 +0100

    Add Filter

---
+++ src/main/java/de/malkusch/lightshow/renderer/infrastructure/transformation/Filter.java
@@ -0,0 +1,44 @@
+package de.malkusch.lightshow.renderer.infrastructure.transformation;
+
+import java.util.function.Function;
+
+import de.malkusch.lightshow.common.model.FrameRate;
+import de.malkusch.lightshow.common.model.Position;
+import de.malkusch.lightshow.renderer.model.AlphaColor;
+import de.malkusch.lightshow.renderer.model.LightId;
+import de.malkusch.lightshow.renderer.model.Transformation;
+
+public final class Filter extends Transformation {
+
+	public static Filter darker(Transformation transformation, double darkness) {
+		if (!(darkness >= 0 && darkness <= 1)) {
+			throw new IllegalArgumentException("Must be within 1 and 0");
+		}
+		return new Filter(transformation, c -> c.withAlpha((int) Math.round(c.alpha() * darkness)));
+	}
+
+	private final Transformation transformation;
+	private final Function<AlphaColor, AlphaColor> filter;
+
+	public Filter(Transformation transformation, Function<AlphaColor, AlphaColor> filter) {
+		super(transformation.lightId(), transformation.start(), transformation.duration());
+		this.transformation = transformation;
+		this.filter = filter;
+	}
+
+	@Override
+	public Transformation with(LightId lightId) {
+		return new Filter(transformation.with(lightId), filter);
+	}
+
+	@Override
+	public Transformation with(Position start) {
+		return new Filter(transformation.with(start), filter);
+	}
+
+	@Override
+	public AlphaColor transform(FrameRate rate, Position position) {
+		return filter.apply(transformation.transform(rate, position));
+	}
+
+}
--- src/main/java/de/malkusch/lightshow/renderer/shows/Carneval.java
+++ src/main/java/de/malkusch/lightshow/renderer/shows/Carneval.java
@@ -12,6 +12,7 @@ import de.malkusch.lightshow.common.model.Duration;
 import de.malkusch.lightshow.common.model.FrameRate;
 import de.malkusch.lightshow.common.model.Position;
 import de.malkusch.lightshow.renderer.infrastructure.transformation.Fade;
+import de.malkusch.lightshow.renderer.infrastructure.transformation.Filter;
 import de.malkusch.lightshow.renderer.infrastructure.transformation.GroupFactory;
 import de.malkusch.lightshow.renderer.infrastructure.transformation.Rainbow;
 import de.malkusch.lightshow.renderer.infrastructure.transformation.RunnerFactory;
@@ -56,52 +57,114 @@ public final class Carneval {
 	}
 
 	public List<Transformation> load() {
-		var highPianoColor = new AlphaColor(new Color(0, 100, 255), 80);
-		var highPianoBlink = Fade.blink(leftCenter.id(), at(0), highPianoColor, duration(0.1), duration(0.2));
+		{
+			var highPianoColor = new AlphaColor(new Color(0, 100, 255), 80);
+			var highPianoBlink = Fade.blink(leftCenter.id(), at(0), highPianoColor, duration(0.1), duration(0.2));
 
-		var pianoEnd = at(24);
+			var pianoEnd = at(24);
 
-		for (var position = at(0); !position.isAfter(pianoEnd); position = position
-				.shift(frameRate.duration(0, random(50, 150)))) {
-			var seq1 = leftToRight.runner(highPianoBlink.with(position), frameRate.duration(0, random(200, 600)));
-			position = position.shift(duration(0.050));
-			var seq2 = rightToLeft.runner(highPianoBlink.with(position), frameRate.duration(0, random(200, 600)));
-			add(Sequence.from(seq1, seq2));
+			for (var position = at(0); !position.isAfter(pianoEnd); position = position
+					.shift(frameRate.duration(0, random(50, 150)))) {
+				var seq1 = leftToRight.runner(highPianoBlink.with(position), frameRate.duration(0, random(200, 600)));
+				position = position.shift(duration(0.050));
+				var seq2 = rightToLeft.runner(highPianoBlink.with(position), frameRate.duration(0, random(200, 600)));
+				add(Sequence.from(seq1, seq2));
+			}
+
+			var highLoudPianoColor = highPianoColor.withAlpha(255);
+			var loudPianoBlink = Fade.blink(leftCenter.id(), at(24.8), highLoudPianoColor, duration(0.150),
+					duration(0.450));
+			add(allLights.grouped(loudPianoBlink));
+
+			var stringColor = red.withAlpha(150);
+			var string1 = leftToRight.runner(
+					Fade.blink(leftCenter.id(), at(6), stringColor, duration(0.100), duration(1)), duration(0.500));
+			var string2 = rightToLeft.runner(
+					Fade.blink(leftCenter.id(), at(8.5), stringColor, duration(0.100), duration(1)), duration(0.500));
+
+			add(string1.withStart(at(6)));
+			add(string2.withStart(at(8.5)));
+			add(string1.withStart(at(10.8)));
+			add(string2.withStart(at(13.2)));
+			add(string1.withStart(at(15.8)));
+
+			var string1Short = rightToLeft.runner(
+					Fade.blink(leftCenter.id(), at(6), stringColor, duration(0.100), duration(1)), duration(0.250));
+
+			var string2Short = leftToRight.runner(
+					Fade.blink(leftCenter.id(), at(6), stringColor, duration(0.100), duration(1)), duration(0.250));
+
+			add(string1Short.withStart(at(17.9)));
+			add(string2Short.withStart(at(18.7)));
+			add(string1Short.withStart(at(19.7)));
+			add(string2Short.withStart(at(20.75)));
+			add(string1Short.withStart(at(21.9)));
+			add(string2Short.withStart(at(23)));
+
+			add(Rainbow.running(at(25.9), duration(0.700), rightToLeft, duration(3)));
 		}
 
-		var highLoudPianoColor = highPianoColor.withAlpha(255);
-		var loudPianoBlink = Fade.blink(leftCenter.id(), at(24.8), highLoudPianoColor, duration(0.150),
-				duration(0.450));
-		add(allLights.grouped(loudPianoBlink));
+		// silence
 
-		var stringColor = red.withAlpha(150);
-		var string1 = leftToRight.runner(Fade.blink(leftCenter.id(), at(6), stringColor, duration(0.100), duration(1)),
-				duration(0.500));
-		var string2 = rightToLeft.runner(
-				Fade.blink(leftCenter.id(), at(8.5), stringColor, duration(0.100), duration(1)), duration(0.500));
+		{
+			var pianoColor = new AlphaColor(new Color(0, 255, 0), 255);
+			var rightPiano = Fade.blink(frontRight.id(), at(0), pianoColor, duration(0.05), duration(0.5));
+			var rightPianoOpen = Fade.blink(rightPiano.lightId(), at(0), pianoColor, duration(0.05), duration(0.9));
+			var darkRightPianoOpen = Filter.darker(rightPianoOpen, 0.5);
+			var leftPianoId = frontLeft.id();
+			var leftPianoOpen = rightPianoOpen.with(leftPianoId);
+			var darkLeftPianoOpen = Filter.darker(leftPianoOpen, 0.5);
 
-		add(string1.withStart(at(6)));
-		add(string2.withStart(at(8.5)));
-		add(string1.withStart(at(10.8)));
-		add(string2.withStart(at(13.2)));
-		add(string1.withStart(at(15.8)));
+			add(rightPiano.with(at(29.600)));
+			add(rightPiano.with(at(30)));
+			add(rightPianoOpen.with(at(30.200)));
 
-		var string1Short = rightToLeft
-				.runner(Fade.blink(leftCenter.id(), at(6), stringColor, duration(0.100), duration(1)), duration(0.250));
+			add(rightPiano.with(at(30.7)));
+			add(rightPiano.with(at(31)));
+			add(rightPianoOpen.with(at(31.200)));
 
-		var string2Short = leftToRight
-				.runner(Fade.blink(leftCenter.id(), at(6), stringColor, duration(0.100), duration(1)), duration(0.250));
+			add(withLights(rightPiano.with(at(31.7)), leftPianoId));
+			add(withLights(rightPiano.with(at(32.07)), leftPianoId));
+			add(withLights(rightPianoOpen.with(at(32.300)), leftPianoId));
 
-		add(string1Short.withStart(at(17.9)));
-		add(string2Short.withStart(at(18.7)));
-		add(string1Short.withStart(at(19.7)));
-		add(string2Short.withStart(at(20.75)));
-		add(string1Short.withStart(at(21.9)));
-		add(string2Short.withStart(at(23)));
+			add(withLights(rightPiano.with(at(32.81)), leftPianoId));
+			add(withLights(rightPiano.with(at(33.1)), leftPianoId));
+			add(withLights(rightPianoOpen.with(at(33.310)), leftPianoId));
 
-		add(Rainbow.running(frameRate.position(25, 900), duration(0.700), rightToLeft, duration(3)));
+			add(withLights(rightPiano.with(at(33.82)), leftPianoId));
+			add(withLights(rightPiano.with(at(34.1)), leftPianoId));
+			add(withLights(rightPianoOpen.with(at(34.35)), leftPianoId));
+
+			add(leftPianoOpen.with(at(34.85)));
+			add(darkRightPianoOpen.with(at(34.85)));
+
+			add(rightPianoOpen.with(at(35.35)));
+			add(darkLeftPianoOpen.with(at(35.35)));
+
+			add(leftPianoOpen.with(at(35.87)));
+			add(darkRightPianoOpen.with(at(35.87)));
+
+			add(rightPianoOpen.with(at(36.4)));
+			add(darkLeftPianoOpen.with(at(36.4)));
+
+			add(leftPianoOpen.with(at(36.93)));
+			add(darkRightPianoOpen.with(at(36.93)));
+
+			add(rightPianoOpen.with(at(37.46)));
+			add(darkLeftPianoOpen.with(at(37.46)));
+
+			add(leftPianoOpen.with(at(38)));
+			add(darkRightPianoOpen.with(at(38)));
+		}
 
 		return transformations;
+	}
+
+	private Sequence withLights(Transformation transformation, LightId... ids) {
+		var idList = new ArrayList<>(asList(ids));
+		idList.add(transformation.lightId());
+		var groups = new GroupFactory(idList);
+		return groups.grouped(transformation);
 	}
 
 	private Duration duration(int seconds) {
@@ -128,6 +191,10 @@ public final class Carneval {
 			millisecond = 0;
 		}
 		return frameRate.position(intSecond, millisecond);
+	}
+
+	private void add(Transformation transformation) {
+		transformations.add(transformation);
 	}
 
 	private void add(Sequence sequence) {
